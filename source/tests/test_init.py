d7f48b729aefa18beea6a1d941878040f6ed72d8
Andrew Govorovskij <govorovskij@gmail.com>
2014-10-03 15:08:14 +0400

    meta checker tests

--- source/tests/test_init.py
+++ source/tests/test_init.py
@@ -1,7 +1,9 @@
 # coding=utf-8
+from bs4 import BeautifulSoup
 import mock
+import re
 import unittest
-from lib import to_unicode, to_str, get_counters, fix_market_url, GOOGLE_MARKET_URL, prepare_url
+from lib import to_unicode, to_str, get_counters, fix_market_url, GOOGLE_MARKET_URL, prepare_url, check_for_meta
 
 
 class LibInitTestCase(unittest.TestCase):
@@ -51,6 +53,47 @@ class LibInitTestCase(unittest.TestCase):
             assert parser.called
             assert url_part.encode.called
 
+
+    def test_check_for_meta_without_meta_tag(self):
+        result = mock.Mock()
+        result.attrs = {}
+        with mock.patch.object(BeautifulSoup, 'find', mock.Mock(return_value=result)):
+            self.assertEqual(check_for_meta('test', 'test'), None)
+
+
+    def test_check_for_meta_with_incorrect_meta_tag(self):
+        result = mock.Mock()
+        result.attrs = {'content': 10}
+        with mock.patch.object(BeautifulSoup, 'find', mock.Mock(return_value=result)):
+            self.assertEqual(check_for_meta('test', 'test'), None)
+
+
+    def test_check_for_meta_with_httpequiv_and_bad_content(self):
+        result = mock.Mock()
+        content = '423432'
+        result.__getitem__ = mock.Mock(return_value=content)
+        result.attrs = {'http-equiv': 'refresh', 'content': 10}
+        with mock.patch.object(BeautifulSoup, 'find', mock.Mock(return_value=result)):
+            self.assertEqual(check_for_meta('test', 'test'), None)
+
+    def test_check_for_meta_with_httpequiv_and_content_url_search_fail(self):
+        result = mock.Mock()
+        content = '423432;url=blabla'
+        result.__getitem__ = mock.Mock(return_value=content)
+        result.attrs = {'http-equiv': 'refresh', 'content': content}
+        with mock.patch.object(BeautifulSoup, 'find', mock.Mock(return_value=result)):
+            with mock.patch.object(re, 'search', mock.Mock(return_value=None)) as re_search:
+                self.assertEqual(check_for_meta('test', 'test'), None)
+                self.assertTrue(re_search.called, 'search for url pattern')
+
+    def test_check_for_meta_with_httpequiv_and_content_url_search_ok(self):
+        result = mock.Mock()
+        url = 'test?ok=go'
+        content = '423432;url=' + url
+        result.__getitem__ = mock.Mock(return_value=content)
+        result.attrs = {'http-equiv': 'refresh', 'content': content}
+        with mock.patch.object(BeautifulSoup, 'find', mock.Mock(return_value=result)):
+            self.assertEqual(check_for_meta('test', 'test'), url)
 
 
 
